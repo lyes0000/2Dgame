ba4e0353275a29bcc77d87ef490659877b822e85
Lyes <hachid14@gmail.com>
2024-11-28 12:54:50 +0100

    Added a new method for starting the game by intializing a Thread that takes the Game Panel as parameter

--- JavaGame/src/main/GamePanel.java
+++ JavaGame/src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     //SCREEN SETTINGS
     final int originalTileSize = 32; //16x16
@@ -15,9 +15,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
