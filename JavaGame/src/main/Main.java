3481478cbb37226e013cef68cfca0ba972b78cfb
HiraethSerra <tresaserrafina@gmail.com>
2024-12-01 15:30:08 +0100

    Created game loop, assigned ASWD as controls and set FPS

--- JavaGame/src/main/Main.java
+++ JavaGame/src/main/Main.java
@@ -1,8 +1,25 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Game!");
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Adventure Time!");
+
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
